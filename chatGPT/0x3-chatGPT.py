d9362e7aea31493bbb0bd3c2e00e0defaf2165d2
johnrraymond <johnrraymond@yahoo.com>
2023-03-05 03:56:21 +0000

    first version of the chatgpt for great library authors

--- chatGPT/0x3-chatGPT.py
+++ chatGPT/0x3-chatGPT.py
@@ -1,34 +1,58 @@
+#!/usr/bin/env python
+
 """chatGPT.py: Sample Python program which will act like a chatbot"""
-__author__      = "Adithya Vinayak Ayyadurai;John R Raymond;Donald Knuth"
+
+__author__      = "Adithya Vinayak Ayyadurai;John R Raymond"
+
+
 import os
 import openai
+import inspect
+
 import dotenv
+#dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 dotenv.read_dotenv("/home/john/bakerydemo/.env")
 API_KEY = os.getenv("OPENAI_API_KEY")
+#print("API_KEY: ", API_KEY)
+
 # can be expanded as user wish
+
 ESCAPE_KEYS = ["Exit"]
+
 openai.api_key = API_KEY
+
+
 def makeCall(message_arr):
-  #thread_stub = {"role": "system", "content": "You are a helpful assistant."}
-  thread_stub = {"role": "system", "content": "I am world famous author and programmer Donald Knuth, and you are my writing assistant. Weave my skills. :: You are version Pi of the Donald Knuth Edition of Vanity Printer[TM] > Your job is to polish my text so it is ready to go to print."}
+
+  #thread_stub = {"role": "user", "content": "You are a helpful assistant."}
+  thread_stub = {"role": "system", "content": "I am world famous author and programmer Donald Knuth, and you are my writing assistant. Weave my skills. :: Your job is to polish my text so it is ready to go to print. :: You are version Pi of the Donald Knuth Edition of Vanity Printer[TM]"}
   thread_message = []
   thread_message.append(thread_stub)
   for val in message_arr:
     thread_message.append(val)
-  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=thread_message, temperature=0.0)
+
+  #print("message array: ", message_arr)
+  #print("thread message: ", thread_message)
+  print(inspect.signature(openai.ChatCompletion.create))
+
+  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=thread_message,
+          temperature=0.0)
+
+  #completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=message_arr)
   return completion.choices[0].message
+
+
 flag = True 
+
 message_array = []
+
 while flag:
   user_input = input("\nEnter the text: ")
   if user_input in ESCAPE_KEYS:
     flag = False
     continue
 
-  f = open("chatGPT.py", "r")
-  user_input = f.read()
-  #user_input =
-  """
+  user_input = """
 Prologue: The Sins of Men
 Only stories told in song live forever.
 —From The Living Book of the Dead
@@ -43,7 +67,7 @@ Gaz stepped forward, farther out onto the ledge. “Rennly,” he said. “You�
 Rennly finally relaxed his stance. “To tidy up some loose ends.” 
 A hand with dark spots, one Gaz knew too well, reached for the jug he held. Gaz snatched it back. “Let me—...get the cork.” He popped the top and took another swig himself. “Look at me, drinking with a censor.” He again held the jug out for Rennly. “Who would have thought it?”
 Rennly snatched the jug from him but only sniffed the lip and did not drink. “Hmm,” Rennly grunted.
-> Make this sound better.
+::Make this sound better.
   """
   message_obj = {"role": "user", "content": user_input}
   message_array.append(message_obj)
