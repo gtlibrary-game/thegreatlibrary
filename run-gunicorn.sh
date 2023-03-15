10ffbc55a233e73d6d0108160d1efb0b35e581a9
John R Raymond <johnrrraymond@yahoo.com>
2023-03-15 03:37:02 +0000

    check for api key.

--- run-gunicorn.sh
+++ run-gunicorn.sh
@@ -1,1 +1,9 @@
+
+if [[ -z "${OPEN_API_KEY}" ]]; then
+	    echo "OPEN_API_KEY is not set"
+	    exit 1
+    else
+	    echo "OPEN_API_KEY is set"
+fi
+
 gunicorn -c gunicorn-cfg.py bakerydemo.openai:application &
