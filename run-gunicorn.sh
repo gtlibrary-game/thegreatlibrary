02510913a5948f416e9d9f83c0402cf2af1ac9d6
John R Raymond <johnrrraymond@yahoo.com>
2023-03-16 03:45:39 +0000

    cleanup the starting env.

--- run-gunicorn.sh
+++ run-gunicorn.sh
@@ -1,9 +1,19 @@
 
-if [[ -z "${OPEN_API_KEY}" ]]; then
-	    echo "OPEN_API_KEY is not set"
+if [[ -z "${OPENAI_API_KEY}" ]]; then
+	    echo "OPENAI_API_KEY is not set"
 	    exit 1
     else
-	    echo "OPEN_API_KEY is set"
+	    echo "OPENAI_API_KEY is set"
 fi
 
-gunicorn -c gunicorn-cfg.py bakerydemo.openai:application &
+if [[ -z "${DD_API_KEY}" ]]; then
+	echo "DD_API_KEY not set"
+	exit 1
+   else
+	echo "DD_API_KEY is set"
+fi
+
+export DD_REMOTE_CONFIGURATION_ENABLED=false
+
+DD_SERVICE=thegreatlibrary DD_ENV=dev DD_VERSION=0.1.0 \
+	 ddtrace-run -p gunicorn -c gunicorn-cfg.py bakerydemo.openai:application &
