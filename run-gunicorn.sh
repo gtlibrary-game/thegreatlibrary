42013144f064f9c702f4d00737c20e793f3ac0bb
John R Raymond <johnrrraymond@yahoo.com>
2023-05-23 18:33:27 +0000

    increased timeouts for ai calls.

--- run-gunicorn.sh
+++ run-gunicorn.sh
@@ -1,9 +1,13 @@
 
 if [[ -z "${OPENAI_API_KEY}" ]]; then
-	    echo "OPENAI_API_KEY is not set"
-	    exit 1
-    else
-	    echo "OPENAI_API_KEY is set"
+	echo "OPENAI_API_KEY is not set"
+	exit 1
+else
+    echo "OPENAI_API_KEY is set"
+    if ! openai api fine_tunes.list; then
+        echo "Failed to run openai api fine_tunes.list"
+        exit 1
+    fi
 fi
 
 if [[ -z "${DD_API_KEY}" ]]; then
@@ -13,7 +17,10 @@ if [[ -z "${DD_API_KEY}" ]]; then
 	echo "DD_API_KEY is set"
 fi
 
+
+
 export DD_REMOTE_CONFIGURATION_ENABLED=false
 
 DD_SERVICE=thegreatlibrary DD_ENV=dev DD_VERSION=0.1.0 \
-	 ddtrace-run -p gunicorn -c gunicorn-cfg.py bakerydemo.openai:application &
+	 ddtrace-run -p gunicorn -c gunicorn-cfg.py bakerydemo.openai:application  \
+	 --keep-alive 3000 -t 3000 --graceful-timeout 3000
